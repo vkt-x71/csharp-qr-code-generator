ecf73471276e89838f1007a6e54b28f890342757
vkt571 <veyselkaranitekgoz@gmail.com>
2021-05-11 23:08:56 +0300

    refactoring

--- QrCodeGenerate/Form1.cs
+++ QrCodeGenerate/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZXing;
+using ZXing.QrCode;
 
 namespace QrCodeGenerate
 {
@@ -15,6 +17,43 @@ namespace QrCodeGenerate
         public Form1()
         {
             InitializeComponent();
+            this.button1.Click += Button1_Click;
+            this.button2.Click += Button2_Click;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            pictureBox1.Image = null;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            QrCodeEncodingOptions options = new QrCodeEncodingOptions
+            {
+                DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = 250,
+                Height = 250,
+            };
+            var writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.QR_CODE;
+            writer.Options = options;
+
+            if (String.IsNullOrWhiteSpace(textBox1.Text) || String.IsNullOrEmpty(textBox1.Text))
+            {
+                pictureBox1.Image = null;
+                MessageBox.Show("Text not found", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                var qr = new ZXing.BarcodeWriter();
+                qr.Options = options;
+                qr.Format = ZXing.BarcodeFormat.QR_CODE;
+                var result = new Bitmap(qr.Write(textBox1.Text.Trim()));
+                pictureBox1.Image = result;
+                
+            }
         }
     }
 }
